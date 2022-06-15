8fe643936172fdea7eff845f18d49ed9576b1702
primawk <pwkuntoro@gmail.com>
2022-06-15 22:32:29 +0700

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,15 @@ import Register from "./pages/Register";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import RequireAuth from "./pages/RequireAuth";
+import Bookings from "./pages/Bookings";
 
 function App() {
   return (
     <Routes>
       <Route element={<RequireAuth />}>
-        <Route path="home" element={<Home />} />
+        <Route path="rooms" element={<Home />}>
+          <Route path="bookings" element={<Bookings />} />
+        </Route>
       </Route>
       <Route path="login" element={<Login />} />
       <Route path="/" element={<Register />} />
